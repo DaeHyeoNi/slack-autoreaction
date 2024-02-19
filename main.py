de2aac2a0ee0b61863eb2052fb8d6d436e0e753a
daehyeoni <daehyeoni3333@gmail.com>
2024-02-20 00:54:09 +0900

    Add weekend check

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import time
 from config import settings
 from modules.slack_monitor import SlackMonitor
 
+
+def is_weekend():
+    current_day = time.localtime().tm_wday
+    return current_day >= 5
+
+
 if __name__ == "__main__":
     monitor = SlackMonitor(
         token=settings.SLACK_USER_TOKEN,
@@ -10,7 +16,8 @@ if __name__ == "__main__":
     )
 
     while True:
-        if settings.WORKING_HOUR_START <= time.localtime().tm_hour < settings.WORKING_HOUR_END:
+        current_day = time.localtime().tm_wday
+        if not is_weekend() and settings.WORKING_HOUR_START <= time.localtime().tm_hour < settings.WORKING_HOUR_END:
             monitor.run(settings.SLACK_USER_ID, last_message_limit=settings.LAST_MESSAGES_LIMIT)
 
         time.sleep(settings.LOOP_INTERVAL)
